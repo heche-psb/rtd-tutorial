9ac3fbcfc789a51c38f99e285b89213787cafa43
heche-psb <heche@HengchiChen.localdomain>
2023-11-17 10:15:11 +0100

    bug fixed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ release = 'v2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.duration','sphinx.ext.doctest','sphinx.ext.autodoc']
+extensions = ['sphinx.ext.duration','sphinx.ext.doctest','sphinx.ext.autodoc', 'sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
